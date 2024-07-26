37d7c0006a95c4d7361bcffba490403b72324d4d
Tushar15797 <tsp15797@gmail.com>
2024-07-26 19:23:25 +0530

    Lab 17 Push

---
+++ src/test/java/A202024/Lab13.java
@@ -0,0 +1,19 @@
+package A202024;
+
+public class Lab13 {
+    public static void main(String[] args) {
+        System.out.println("112234");
+        main1();
+    }
+    public static void main1() {
+        int i=1;
+        int a=2;
+        try{
+            System.out.println(a);
+        }
+        catch (Exception e){
+            System.out.println(i);
+        }
+    }
+}
+
